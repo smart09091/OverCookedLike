119551cee96d4dfca35722c7e1933b6fa8e5e9ad
Adrian Alcomendras <cloud102000@gmail.com>
2023-03-28 13:51:42 +0800

    Finished making clear counters with functionality for identifying which counter is currently select and base functionality for interacting with clear counters

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set;}
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+
+    //Allows you to make a generic class that can be used to pass an argument to an event handler
+    public class OnSelectedCounterChangedEventArgs : EventArgs{
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private float moveSpeed = 7;
     [SerializeField] private GameInput gameInput;
-
+    [SerializeField] private LayerMask countersLayerMask;
+ 
     private bool isWalking;
+    private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+    private void Awake(){
+        if(Instance != null){
+            Debug.LogError("There is more than one Player Instance!");
+        }
+        Instance = this;
+    }
+
+    private void Start(){
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e){
+        if(selectedCounter != null){
+            selectedCounter.Interact();
+        }
+    }
 
     // Update is called once per frame
     private void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking(){
+        return isWalking;
+    }
+
+    private void HandleInteractions(){
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if(moveDir != Vector3.zero){
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycasyHit, interactDistance, countersLayerMask)){
+            if(raycasyHit.transform.TryGetComponent(out ClearCounter clearCounter)){
+                //clearCounter.Interact();
+                if(clearCounter != selectedCounter){
+                    SetSelectedCounter(clearCounter);
+                    
+                }
+            }else{
+                SetSelectedCounter(null);
+
+            }
+        }else{
+            SetSelectedCounter(null);
+
+        }
+
+    }
+
+    private void HandleMovement(){
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         
@@ -49,7 +114,11 @@ public class Player : MonoBehaviour
         transform.forward  = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
-    public bool IsWalking(){
-        return isWalking;
+    private void SetSelectedCounter(ClearCounter selectedCounter){
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs{
+            selectedCounter = selectedCounter
+        });
     }
 }
